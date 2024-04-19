87ef96ae5b3f1d69314a88b46c4716dbbef8ce8c
Aniltiwari1212 <121211457+Aniltiwari1212@users.noreply.github.com>
2024-04-19 10:51:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+var owl = $('.prodSlide');
+owl.owlCarousel({
+    items:3, 
+  // items change number for slider display on desktop
+  
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true
+});
+
+var owl = $('.owl-carouseltwo');
+owl.owlCarousel({
+    items:5, 
+  // items change number for slider display on desktop
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true
+});
+
+
